4dd0025a9af4deb2a44c6bb1527d287740f73872
Sasha Korn <sashadasasha@gmail.com>
2024-02-22 17:45:28 +0300

    refactoring after linter recomendation

--- speech_syntese.py
+++ speech_syntese.py
@@ -1,13 +1,13 @@
-import torch
 import re
 from transformers import AutoTokenizer, AutoModelWithLMHead
 
-tokenizer = AutoTokenizer.from_pretrained('tinkoff-ai/ruDialoGPT-medium')
-model = AutoModelWithLMHead.from_pretrained('tinkoff-ai/ruDialoGPT-medium')
+tokenizer = AutoTokenizer.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
+model = AutoModelWithLMHead.from_pretrained("tinkoff-ai/ruDialoGPT-medium")
+
 
 def generate_answer(input_question):
-    prepared_input = "@@ПЕРВЫЙ@@ " + input_question + " @@ВТОРОЙ@@ " 
-    inputs = tokenizer(prepared_input, return_tensors='pt')
+    prepared_input = "@@ПЕРВЫЙ@@ " + input_question + " @@ВТОРОЙ@@ "
+    inputs = tokenizer(prepared_input, return_tensors="pt")
     generated_token_ids = model.generate(
         **inputs,
         top_k=10,
@@ -22,7 +22,12 @@ def generate_answer(input_question):
         eos_token_id=50257,
         max_new_tokens=40
     )
-    context_with_response = [tokenizer.decode(sample_token_ids) for sample_token_ids in generated_token_ids]
-    answer = re.sub('@@ПЕРВЫЙ@@.*?@@ВТОРОЙ@@','',context_with_response[0], flags=re.DOTALL)
-    return answer.replace("@@ПЕРВЫЙ@@", '').replace("@@ВТОРОЙ@@", '')
-    
+    context_with_response = [
+        tokenizer.decode(sample_token_ids)
+        for sample_token_ids in generated_token_ids
+    ]
+    answer = re.sub(
+        "@@ПЕРВЫЙ@@.*?@@ВТОРОЙ@@", "",
+        context_with_response[0], flags=re.DOTALL
+    )
+    return answer.replace("@@ПЕРВЫЙ@@", "").replace("@@ВТОРОЙ@@", "")
--- web_form.py
+++ web_form.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import speech_syntese as ss
 
+
 def main():
-    st.title('Добро пожаловать в наш чат. Спросите меня что-нибудь')
+    st.title("Добро пожаловать в наш чат. Спросите меня что-нибудь")
     prompt = st.chat_input(placeholder="Начните разговор")
 
     if "messages" not in st.session_state:
@@ -21,8 +22,10 @@ def main():
             message_placeholder = st.empty()
             full_response = ss.generate_answer(prompt)
             message_placeholder.markdown(full_response)
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": full_response}
+        )
 
-       
+
 if __name__ == "__main__":
     main()
